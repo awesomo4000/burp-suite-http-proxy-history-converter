103cdd11fe65c64d9f04a2cbfbd2a12742ce376d
Junki Kizuka <k.falconws@gmail.com>
2020-05-11 11:45:50 +0900

    Change output columns.

--- convert-burp-suite-http-proxy-history-to-csv.py
+++ convert-burp-suite-http-proxy-history-to-csv.py
@@ -39,45 +39,17 @@ def convert_to_output_file(http_history, format_handler):
     with io.open(format_handler.filename, 'w', encoding='utf-8', newline='') as output_file:
         format_handler.set_output_file(output_file)
         format_handler.header_prefix()
-        format_handler.header_column('Time')
+        format_handler.header_column('No.')  # Comment as No.
+        format_handler.header_column('Name')
         format_handler.header_column('URL')
-        format_handler.header_column('Hostname')
-        format_handler.header_column('IP address')
-        format_handler.header_column('Port')
-        format_handler.header_column('Protocol')
         format_handler.header_column('Method')
-        format_handler.header_column('Path')
-        format_handler.header_column('Extension')
-        format_handler.header_column('Request')
-        format_handler.header_column('Status')
-        format_handler.header_column('Response length')
-        format_handler.header_column('MIME type')
-        format_handler.header_column('Response')
-        format_handler.header_column('Comment')
         format_handler.header_suffix()
         for line in http_history['items']['item']:
             format_handler.row_prefix()
-            format_handler.row_column(line['time'])
-            format_handler.row_column(line['url'])
-            format_handler.row_column(line['host']['#text'])
-            format_handler.row_column(line['host']['@ip'])
-            format_handler.row_column(line['port'])
-            format_handler.row_column(line['protocol'])
-            format_handler.row_column(line['method'])
-            format_handler.row_column(line['path'])
-            format_handler.row_column(line['extension'])
-            if '#text' in line['request']:
-                format_handler.row_column(line['request']['#text'], encoded=True)
-            else:
-                format_handler.row_column("None")
-            format_handler.row_column(line['status'])
-            format_handler.row_column(line['responselength'])
-            format_handler.row_column(line['mimetype'])
-            if '#text' in line['response']:
-                format_handler.row_column(line['response']['#text'], encoded=True)
-            else:
-                format_handler.row_column("None")
             format_handler.row_column(line['comment'])
+            format_handler.row_column('')  # Blank Name column
+            format_handler.row_column(line['url'])
+            format_handler.row_column(line['method'])
             format_handler.row_suffix()
         format_handler.footer()
 
